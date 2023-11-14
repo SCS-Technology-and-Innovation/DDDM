6fc03dbbc159b680c6118163770ea4e290041486
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-11-14 16:32:23 -0500

    First drafts for Jordan

---
+++ COIN/boolean/tex2gif.sh
@@ -0,0 +1,8 @@
+speed=100 # slow ones
+
+for file in `ls -1 *.tex`;
+do
+    pdflatex $file
+    label=`basename $file .tex`
+    convert -density 300 -delay $speed -loop 0 -background white -alpha remove ${label}.pdf ${label}.gif
+done
