ef454683ec77c95dca649977674ad1cfb8d6caa2
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-11-15 14:33:36 -0500

    An evaluation

--- COIN/boolean/tex2gif.sh
+++ COIN/boolean/tex2gif.sh
@@ -1,8 +1,9 @@
-speed=100 # slow ones
+speed=150 # slow ones
 
 for file in `ls -1 *.tex`;
 do
     pdflatex $file
+    pdflatex $file # twice for internal labels
     label=`basename $file .tex`
     convert -density 300 -delay $speed -loop 0 -background white -alpha remove ${label}.pdf ${label}.gif
 done
@@ -10,3 +11,5 @@ rm *.aux
 rm *.log
 rm *.out
 rm *.snm
+rm *.nav
+rm *.toc
