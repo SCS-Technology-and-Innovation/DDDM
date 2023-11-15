96b172ccc257862d9040a1e17e86cda52aefc19f
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-11-15 14:58:40 -0500

    Accessibility improvements

--- COIN/boolean/tex2gif.sh
+++ COIN/boolean/tex2gif.sh
@@ -2,10 +2,14 @@ speed=150 # slow ones
 
 for file in `ls -1 *.tex`;
 do
-    pdflatex $file
-    pdflatex $file # twice for internal labels
-    label=`basename $file .tex`
-    convert -density 300 -delay $speed -loop 0 -background white -alpha remove ${label}.pdf ${label}.gif
+    if [ "$file" = "alldefs.tex" ]; then
+	echo "Skipping the definition file"
+    else
+	pdflatex $file
+	pdflatex $file # twice for internal labels
+	label=`basename $file .tex`
+	convert -density 300 -delay $speed -loop 0 -background white -alpha remove ${label}.pdf ${label}.gif
+    fi
 done
 rm *.aux
 rm *.log
