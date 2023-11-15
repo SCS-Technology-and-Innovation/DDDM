afd45f8fb6fa4c87fe9a2592db9b9676141fa437
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-11-15 12:39:07 -0500

    More operators

--- COIN/boolean/tex2gif.sh
+++ COIN/boolean/tex2gif.sh
@@ -6,3 +6,7 @@ do
     label=`basename $file .tex`
     convert -density 300 -delay $speed -loop 0 -background white -alpha remove ${label}.pdf ${label}.gif
 done
+rm *.aux
+rm *.log
+rm *.out
+rm *.snm
