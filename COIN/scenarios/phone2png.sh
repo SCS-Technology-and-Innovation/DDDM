2b3d7f702e0bfe6e7917cf39eb86435f122bed76
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-11-24 14:53:15 -0500

    more automatic

--- COIN/scenarios/phone2png.sh
+++ COIN/scenarios/phone2png.sh
@@ -9,3 +9,11 @@ do
     fn=`basename $file .pdf`
     convert -density 300 $file -resize 25% $fn.png
 done
+
+rm *.aux
+rm *.log
+rm *.out
+
+git add *.png
+git add *.tex
+git commit -m 'phone updates'
