8d6adb035f7aa6d4dc56eb1be1de361225787a15
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2023-11-30 17:34:33 -0500

    Filename issues

--- COIN/avi2other.sh
+++ COIN/avi2other.sh
@@ -2,6 +2,11 @@
 ffmpeg -i formation.avi -c:v mpeg4 formation.mp4
 
 # animated GIF
-ffmpeg -i formation.avi -t 10 tmp%02d.gif
-gifsicle --delay=5 --loop tmp*.gif > formation.gif
+ffmpeg -r 1 -i formation.mp4 -r 1 "tmp%03d.gif"
+gifsicle --delay=5 --loop tmp*.gif > formationLARGE.gif
 rm tmp*.gif
+
+# same but with imagemagick (slower, bigger file)
+#ffmpeg -r 1 -i formation.mp4 -r 1 "tmp%03d.png"
+#convert -delay 5 -loop 0 tmp*.png formation.gif
+#rm tmp*.png
